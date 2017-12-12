d0e49b6d35f225ec3b6cc31d302ab0fc1fe829af
Tom Hoad <t.hoad.88@gmail.com>
2017-12-12 14:56:17 +0000

    Make App.jsx a stateless functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   HashRouter,
   Route,
@@ -11,36 +11,26 @@ import About from './containers/About';
 
 import reactLogo from './assets/React-icon.png';
 
-/**
- * this container is defined as class so you can modify state
- * when you add more stuff to it
- */
-class App extends Component {
-  /**
-   * this is our statefull render
-   * @return {objects} our stateless components
-   */
-  render() {
-    return (
-      <HashRouter>
-        <main className="container">
-          <div>
-            <h1>hello world!</h1>
-            <img className="container__image" alt="react logo" src={reactLogo} />
-            <p>If you see this everything is working!</p>
-          </div>
-          <ul className="left">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-          </Switch>
-        </main>
-      </HashRouter>
-    );
-  }
+const App = () => {
+  return (
+    <HashRouter>
+      <main className="container">
+        <div>
+          <h1>hello world!</h1>
+          <img className="container__image" alt="react logo" src={reactLogo} />
+          <p>If you see this everything is working!</p>
+        </div>
+        <ul className="left">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </main>
+    </HashRouter>
+  );
 }
 
 export default App;
