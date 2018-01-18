8fdc515c13802d7336700820219f1aa3516a22bf
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-01-18 19:41:04 +0100

    Extract Routes into own file
    + QOL on /containers for easier imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
 import React from 'react';
-import {
-  HashRouter,
-  Route,
-  Link,
-  Switch,
-} from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 
-import Home from './containers/Home';
-import About from './containers/About';
+import Router from './Router';
 
 import reactLogo from './assets/React-icon.png';
 
 const App = () => (
-    <HashRouter>
-      <main className="container">
-        <div>
-          <h1>hello world!</h1>
-          <img className="container__image" alt="react logo" src={reactLogo} />
-          <p>If you see this everything is working!</p>
-        </div>
-        <ul className="left">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-        </Switch>
-      </main>
-    </HashRouter>
+  <BrowserRouter>
+    <main className="container">
+      <div>
+        <h1>hello world!</h1>
+        <img className="container__image" alt="react logo" src={reactLogo} />
+        <p>If you see this everything is working!</p>
+      </div>
+      <ul className="left">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Router />
+    </main>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/Router.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Home, About } from './containers';
+
+const Router = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+  </Switch>
+);
+
+export default Router;
