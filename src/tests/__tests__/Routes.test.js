360e0bd0d8001d2d6ec5b8b12985559e97b11d96
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-01-19 12:48:52 +0100

    Refactor Routes test

--- src/tests/__tests__/Routes.test.js
+++ src/tests/__tests__/Routes.test.js
@@ -4,20 +4,22 @@ import { MemoryRouter } from 'react-router-dom';
 import Routes from './../../Routes';
 import { Home, About } from './../../containers';
 
-test('default path should redirect to Home component', () => {
-  const wrapper = mount(
-    <MemoryRouter initialEntries={['/']} initialIndex={0}>
-      <Routes />
-    </MemoryRouter>,
-  );
-  expect(wrapper.find(Home)).toHaveLength(1);
-});
+describe('Routes', () => {
+  it('default path should redirect to Home component', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/']} initialIndex={0}>
+        <Routes />
+      </MemoryRouter>,
+    );
+    expect(wrapper.find(Home)).toHaveLength(1);
+  });
 
-test('/about path should redirect to About component', () => {
-  const wrapper = mount(
-    <MemoryRouter initialEntries={['/about']} initialIndex={0}>
-      <Routes />
-    </MemoryRouter>,
-  );
-  expect(wrapper.find(About)).toHaveLength(1);
+  it('/about path should redirect to About component', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/about']} initialIndex={0}>
+        <Routes />
+      </MemoryRouter>,
+    );
+    expect(wrapper.find(About)).toHaveLength(1);
+  });
 });
