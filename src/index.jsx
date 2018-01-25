ca955411382fe1f4b4dd11561b18fca8d172eb59
sirjmkitavi <joseph.kitavi@andela.com>
2018-01-25 19:13:51 +0300

    Update comments in index.jsx
    This is an amazing React Boilerplate and it will be nice if the documentation is EPIC.
    Here's my attempt to ensure of that.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /*
   issue with react-hot-loader
-  even tho those 2 deps are only used in development
+  eventhough those 2 dependencies are only used in development
   eslint has no way to tell that and outputs an error
 */
 
-// react deps
+// react dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
 // hot reload for development
