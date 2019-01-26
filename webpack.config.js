3e10ce520c99f3c40d26ed867b40168b341e220d
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2019-01-26 12:41:10 +0100

    Add fetch polifyll

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     historyApiFallback: true,
   },
-  entry: ['@babel/polyfill', 'react-hot-loader/patch', path.join(__dirname, '/src/index.jsx')],
+  entry: ['@babel/polyfill', 'whatwg-fetch', 'react-hot-loader/patch', path.join(__dirname, '/src/index.jsx')],
   module: {
     rules: [
       {
